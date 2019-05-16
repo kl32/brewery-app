b76adec8a2696718c90d81b5c2d65df9304b24d9
Kiley Lewis <kileylewis32@gmail.com>
2019-05-15 20:56:47 -0500

    update

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 require('dotenv').config()
+
+// http://api.brewerydb.com/v2/brewery/nHLlnK/locations/?key=37ee639f0d9e9ead471687f7a5f3371f
+// http://api.brewerydb.com/v2/brewery/random?key=37ee639f0d9e9ead471687f7a5f3371f
+
+//Build an app that allow a user to find breweries near them.
+const randomBrew = "https://sandbox-api.brewerydb.com/v2/brewery/random?key=37ee639f0d9e9ead471687f7a5f3371f";
+function get() {
+    fetch(randomBrew);
+
+}
+//Remember the Geolocation API you learned in 101!
+
+//Allow users to heart their favorite places.
